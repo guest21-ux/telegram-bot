c4dd5e0ece26c8af2a0c834b42a75e6e620c15df
guest21-ux <erencacabala21@gmail.com>
2026-02-23 19:53:23 +0800

    Update config.py with environment variable checks

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ import os
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 if not TOKEN:
-
     raise RuntimeError("TELEGRAM_TOKEN belum di-set")
-GEMINI_KEY = AIzaSyAonn5GypthQQV3N3Q0HCXeGyViyRG_yMA
+
+GEMINI_KEY = os.getenv("GEMINI_KEY")
+
+if not GEMINI_KEY:
+    raise RuntimeError("GEMINI_KEY belum di-set")
